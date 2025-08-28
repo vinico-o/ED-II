eea924c80f45b7a169da7d139f35786177e73de0
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-08-28 11:43:42 -0300

    feat: add InsercaoNaoCheio function

--- ArvoreB/arvoreB.c
+++ ArvoreB/arvoreB.c
@@ -47,3 +47,36 @@ No* InsercaoCLRS(No* raiz, int chave)
     }
 }
 
+No* InsercaoNaoCheio(No* raiz, int chave)
+{
+    int i;
+    if(raiz->folha == true)
+    {
+        //percorre o vetor ao contrario, passando as chaves uma posicao para frente
+        //desse modo, quando for o local correto para inserir a chave, havera um espaco vazio
+        for(i = raiz->numero; i >= 0 && chave < raiz->chave[i]; i--)
+        {
+            raiz->chave[i + 1] = raiz->chave[i];
+        }
+
+        //inserimos a chave e atualizamos o valor de chaves atual
+        raiz->chave[i + 1] = chave;
+        raiz->numero += 1;
+    }
+    else
+    {
+        //encontrando o ponteiro para prosseguir corretamnete pelo valor da cheve
+        for(i = raiz->numero; i >= 0 && chave < raiz->chave[i]; i--);
+        i++;
+        //OBS: REVER FUNCIONAMENTO DESSE IF
+        if(raiz->filho[i]->numero == MAX)
+        {
+            //TODO:SPLITCHILDREN
+            if(chave > raiz->chave[i])
+            {
+                i++;
+            }
+        }
+        InsercaoNaoCheio(raiz->filho[i], chave);
+    }
+}
