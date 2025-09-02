4860bd11f1c6b55f67a787e9c7771626b9d1c881
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-09-02 13:56:10 -0300

    feat: ajust types of functions to void

--- ArvoreB/arvoreB.c
+++ ArvoreB/arvoreB.c
@@ -24,30 +24,31 @@ No* BuscaRecursiva(No* raiz, int busca)
     }
 }
 
-No* CriarNo(int chave)
+No* CriarNo()
 {
     No* novo = malloc(sizeof(No));
     novo->numero = 0;
     novo->folha = false;
 }
 
-No* InsercaoCLRS(No* raiz, int chave)
+void InsercaoCLRS(Arvore raiz, int chave)
 {
     //se o vetor estiver cheio (2t - 1)
     if(raiz->numero == MAX)
     {
-        No* novo = CriarNo(chave);
+        No* novo = CriarNo();
         novo->filho[0] = raiz;
-        //TODO: SPLITCHILD
-        //TODO: INSERENAOCHEIO
+        
+        SplitChildren(novo, 0);
+        InsercaoNaoCheio(novo, chave);
     }
     else
     {
-        //TODO: INSERENAOCHEIO
+        InsercaoNaoCheio(raiz, chave);
     }
 }
 
-No* InsercaoNaoCheio(No* raiz, int chave)
+void InsercaoNaoCheio(Arvore raiz, int chave)
 {
     int i;
     if(raiz->folha == true)
@@ -65,18 +66,68 @@ No* InsercaoNaoCheio(No* raiz, int chave)
     }
     else
     {
-        //encontrando o ponteiro para prosseguir corretamnete pelo valor da cheve
+        //encontrando o ponteiro para prosseguir corretamnete pelo valor da chave
         for(i = raiz->numero; i >= 0 && chave < raiz->chave[i]; i--);
         i++;
-        //OBS: REVER FUNCIONAMENTO DESSE IF
+        
+        //Se o numero de chaves for igual ao maximo (2t - 1)
         if(raiz->filho[i]->numero == MAX)
         {
-            //TODO:SPLITCHILDREN
+            SplitChildren(raiz, i);
+            //Se a chave for maior que a chave do no pai
             if(chave > raiz->chave[i])
             {
-                i++;
+                i++; //o ponteiro eh ajustado para o filho correto
             }
         }
         InsercaoNaoCheio(raiz->filho[i], chave);
     }
+}
+
+void SplitChildren(Arvore raiz, int index)
+{
+    No* novo = CriarNo(); //Aloca o novo no na memoria
+    No* filho = raiz->filho[index];
+    novo->folha = filho->folha;
+    novo->numero = MIN;
+
+    //copia as t - 1 ultimas chaves para o novo no, ainda em order crescente
+    for(int i = 0; i < MIN; i++)
+    {
+        novo->chave[i] = filho->chave[i + t];
+    }
+
+    //Se o no filho de x for um no interno
+    if(filho->folha == false)
+    {
+        //copiamos os filhos dele para o novo no
+        for(int i = 0; i < t; i++)
+        {
+            novo->filho[i] = filho->filho[i + t];
+        }
+    }
+
+    //como a copia da segunda parte do no filho ja foi copiada,
+    //ajustamos o numero de chaves
+    filho->numero = MIN;
+
+    //Ajusta os ponteiros da raiz para adicionar o novo no
+    for(int i = (raiz->numero + 1); i >= index + 1; i--)
+    {
+        raiz->filho[i + 1] = raiz->filho[i];
+    }
+
+    //como no novo foi copiado os ultimos elementos, ele eh o ultimo filho
+    raiz->filho[index + 1] = novo;
+
+    //Faz o deslocamento das chaves para incluir a mediana no no pai (raiz)
+    for(int i = raiz->numero; i >= index; i--)
+    {
+        raiz->chave[i + 1] = raiz->chave[i];
+    }
+
+    //passamos a mediana para o no pai e ajustamos seu numero de chaves
+    raiz->chave[index] = filho->chave[t]; //utilizamos t pois eh justamente a mediana
+    raiz->numero ++;
+
 }
