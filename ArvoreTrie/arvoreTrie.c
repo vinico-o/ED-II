bbabe11cb867cd1237f4e13a7e7ff985de21c587
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-09-08 10:52:19 -0300

    feat: add Inserir and InserirRecursivo functions

--- ArvoreTrie/arvoreTrie.c
+++ ArvoreTrie/arvoreTrie.c
@@ -9,7 +9,7 @@ unsigned int getBit(unsigned chave, int k)
 No* CriarNo(unsigned chave)
 {
     No* novo = malloc(sizeof(No));
-    novo->folha = true;
+    novo->folha = false;
     novo->esquerda = NULL;
     novo->direita = NULL;
     novo->chave = chave;
@@ -39,4 +39,46 @@ No* BuscaRecursiva(No* arvore, unsigned chave, int nivel)
     {
         return BuscaRecursiva(arvore->esquerda, chave, nivel + 1);
     }
+}
+
+No* Busca(No* arvore, unsigned chave)
+{
+    //Se o elemento for encontrado, retorna o no em que esta, senao retorna NULL;
+    No* resultado = BuscaRecursiva(arvore, chave, 0);
+    return resultado;
+}
+
+No* InserirRecursivo(No* arvore, unsigned chave, int nivel)
+{
+    //Se arovre for nula, cria o No (return), para gerar o caminho
+    //Utilizamos UINT_MAX pois a informacao nao sera relevante
+    if(arvore == NULL)
+    {
+        No* novo = CriarNo(UINT_MAX);
+    }
+
+    //se percorremos toda a arvore, criamos o no com informacao relevante
+    if(nivel == NBITS)
+    {
+        No* novo = CriarNo(chave);
+        novo->folha = true;
+        return novo;
+    }
+
+    //caminhamos pela arvore, a partir da chave 0->esquerda, 1->direita
+    if(getBit(chave, nivel) == 0)
+    {
+        arvore->esquerda = InserirRecursivo(arvore->esquerda, chave, nivel + 1);
+    }
+    else
+    {
+        arvore->direita = InserirRecursivo(arvore->direita, chave, nivel + 1);
+    }
+
+    //retornamos arvore para manter as ligacoes dos ponteiros
+    return arvore;
+}
+
+void Inserir(No** raiz, unsigned int chave) {
+    *raiz = inserir_recursivo(*raiz, chave, 0);
 }
